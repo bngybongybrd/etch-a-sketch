e88c1e031ec8b534f00ce7e51dec557c1aad3284
yx <48951196+bngybongybrd@users.noreply.github.com>
2025-08-02 10:45:11 +0000

    Fixed initial grid not able to trigger eventlistener, made sure the grid sizing is fixed

--- static/script.js
+++ static/script.js
@@ -1,43 +1,65 @@
 const slider = document.querySelector("#gridRange")
 const gridSize = document.querySelectorAll("#gridValue")
 const gridContainer = document.createElement("div")
+gridContainer.classList.add("gridContainer")
 
-console.log(gridSize)
+const MAX_GRID_SIZE = 512
+
+let isMouseDown = false
+
+
 slider.addEventListener("input", () => {
     gridSize.forEach(span => {
         span.textContent = slider.value
     })
 })
 
-for (let i = 0; i < 16; i++) {
-    let row = document.createElement("div")
-    for (let j = 0; j < 16; j++) {
-        const square = document.createElement("div")
-        square.classList.add("square")
-        row.append(square)
-        row.classList.add("row")
+
+slider.addEventListener("change", () => {
+    // remove previous grid and create new
+    document.body.querySelector(".gridContainer").remove()
+    createGrid()
+})
+
+
+function createGrid() {
+    // create new grid
+    const gridContainer = document.createElement("div")
+    gridContainer.classList.add("gridContainer")
+    let size = slider.value
+    for (let i = 0; i < size; i++) {
+        let row = document.createElement("div")
+        for (let j = 0; j < size; j++) {
+            const square = document.createElement("div")
+            square.classList.add("square")
+            square.style.width = square.style.height = `${MAX_GRID_SIZE / size}px`
+            row.append(square)
+            row.classList.add("row")
+            // row.style.height = `${MAX_GRID_SIZE / size}px`
+        }
+        gridContainer.append(row)
     }
-    gridContainer.append(row)
+    const body = document.querySelector("body")
+    body.append(gridContainer)
+
+    // Change colour
+    gridContainer.addEventListener("mousedown", (e) => {
+        const selectedSquare = e.target.closest(".square")
+        isMouseDown = true
+        selectedSquare.style.background = "black"
+    })
+
+
+    gridContainer.addEventListener("mouseover", (e) => {
+        if (isMouseDown === true) {
+            const selectedSquare = e.target.closest(".square")
+            selectedSquare.style.background = "black"
+        }
+    })
+
+
+    gridContainer.addEventListener("mouseup", () => isMouseDown = false)
 }
 
-const body = document.querySelector("body")
-body.append(gridContainer)
 
-// Change colour
-let isMouseDown = false
-
-gridContainer.addEventListener("mousedown", (e) => {
-    const selectedSquare = e.target.closest(".square")
-    // console.log(selectedSquare)
-    isMouseDown = true
-    selectedSquare.style.background = "black"
-})
-
-gridContainer.addEventListener("mouseover", (e) => {
-    if (isMouseDown === true) {
-        const selectedSquare = e.target.closest(".square")
-        selectedSquare.style.background = "black"
-    }
-})
-
-gridContainer.addEventListener("mouseup", () => isMouseDown = false)
+createGrid()
